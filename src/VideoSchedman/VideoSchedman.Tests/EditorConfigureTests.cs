87e7d4263d5945bc6ee6599b0252e8a0fee46865
Валентин <ilyaokubev@gmail.com>
2022-02-16 21:07:59 +0300

    Feature[Video]: накидал базовой архитектуры новому проекту и создал первоначальные тесты

---
+++ src/VideoSchedman/VideoSchedman.Tests/EditorConfigureTests.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VideoSchedman.Tests
+{
+    [TestClass]
+    public class EditorConfigureTests
+    {
+        [TestMethod]
+        public void Configure_SetInputFilesInfos_ValidInputConfig()
+        {
+        }
+
+        [TestMethod]
+        public void Configure_GetMetaInfoFromInputFiles_ValidMetaInfos()
+        {
+
+        }
+
+        [TestMethod]
+        public void Configure_SetOutputMetaInfo_ValidMetaInfo()
+        {
+
+        }
+    }
+}
---
+++ src/VideoSchedman/VideoSchedman.Tests/ScriptBuilderTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VideoSchedman.Tests
+{
+    [TestClass]
+    public class ScriptBuilderTests
+    {
+        [TestMethod]
+        public void Build_InpAndOutConfigurs_ConvertToExtensionCommand()
+        {
+
+        }
+
+        [TestMethod]
+        public void Build_InpAndOutConfigurs_ConcatManyVideosCommand()
+        {
+
+        }
+    }
+}
